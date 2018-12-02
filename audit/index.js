26f20beb92164c5fd58d9d3e2fc679c8fb229c5c
Duncan Lewis <duncan@piranhr.com>
2018-12-02 00:35:40 +0000

    refactor(audit): cleanup the audit function

--- audit/index.js
+++ audit/index.js
@@ -11,57 +11,63 @@ module.exports.handler = (event, context, callback, chrome) => {
         disableCpuThrottling: true,
         disableNetworkThrottling: true
     }
-    console.log(flags)
 
     lighthouse(event.target, flags).then(function(results) {
-        console.log('timestamp: ' + results.generatedTime)
-        console.log('target: ' + results.url)
-        console.log('total-time: ' + results.timing.total)
-        console.log('score: ' + results.score)
-        
         ddb = new AWS.DynamoDB({
             apiVersion: '2012-10-08'
         });
 
-        s3 = new AWS.S3();
+        s3 = new AWS.S3()
 
-        const s3key = `${uuidv5.URL(event.target)}/${uuidv4}`
+        const hash = uuidv4()
+        const s3key = `${uuidv5(event.target, uuidv5.URL)}/${hash}`
         const bucketName = 'lighthouse-auditor-dev-resultsbucket-b04a5n6lygnz'
-
-        s3.putObject({
-            Bucket: bucketName,
-            Key: s3key,
-            Body: JSON.stringify(results),
-            ContentType: 'application/json'
-        });
         
         const date = new Date();
-    
+
         var params = {
-            TableName: 'lighthouse-auditor--dev-auditsTable',
-            Item: {
-                'id': {
-                    S: uuidv4()
-                },
-                'url': {
-                    S: results.url
-                },
-                'score': {
-                    S: results.score.toString()
-                },
-                's3Key': {
-                    S: `s3://${bucketName}/${s3key}`
-                },
-                'time': {
-                    N: date.getTime().toString()
+            s3: {
+                Bucket: bucketName,
+                Key: s3key,
+                Body: JSON.stringify(results),
+                ContentType: 'application/json'
+            },
+            ddb: {
+                TableName: 'lighthouse-auditor--dev-auditsTable',
+                Item: {
+                    'id': {
+                        S: uuidv4()
+                    },
+                    'url': {
+                        S: results.url
+                    },
+                    'score': {
+                        S: results.score.toString()
+                    },
+                    's3Key': {
+                        S: `s3://${bucketName}/${s3key}`
+                    },
+                    'time': {
+                        N: date.getTime().toString()
+                    }
                 }
             }
-        };
+        }
 
         return params
     }).then(function(params) {
-        ddb.putItem(params, function (err, data) {
-            if (err) {
+        s3.putObject(params.s3, function (err, data) {
+            if (err) {  
+                console.log("Error", err);
+                callback(err);
+            } else {
+                console.log("Success", data);
+                callback(null, data);
+            }
+        });
+    }).then(function(params) {
+        ddb.putItem(params.ddb, function (err, data) {
+            if (err) {  
                 console.log("Error", err);
                 callback(err);
             } else {
